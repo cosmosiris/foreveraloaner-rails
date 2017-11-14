14f4aafa4c108f4dced058b3da585845f85c6abb
ikn02010 <ikn02010@gmail.com>
2017-11-13 18:25:41 -0800

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
+
+  devise_for :users 
+
+  namespace :api do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :users do
+      resources :reviews
+    end 
+
+    resources :conversations do
+      resources :messages
+    end
+
+    resources :posts do
+      resources :transactions
+      resources :tags
+    end
+
+    resources :categories, only: [:index, :show] do
+      resources :posts, only: [:index, :show]
+    end
+
+    # resources :search, only: [:index]
+    get '/search', :to => 'search#index'
+  end
+
+  
+  root 'categories#index'
 end
