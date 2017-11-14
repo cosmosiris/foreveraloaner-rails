46f2566fbb4ad973dc34cbaf93c098e7de4705a5
Britt Flowers <brittflowersmusic@gmail.com>
2017-11-14 11:05:04 -0800

    Update search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,20 @@
 class SearchController < ApplicationController
   def index
-    @posts = Post.where(['lower(title) LIKE ?', "%#{params[:query].downcase}%"])
-    render json: @posts
+    @posts = Post.search(params[:term])
+  end
+
+  def self.search(term)
+    if term
+      where('lower(title) LIKE ? AND lower(tag) LIKE ?', "%#{params[:term].downcase}%").order('id DESC')
+    else
+      order('id DESC')
+    end
+  end
+
+
+  private
+
+  def search_params
+    params.require(:search).permit(:term)
   end
 end
