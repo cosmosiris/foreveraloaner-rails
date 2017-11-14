5871ae6ba86c65da0e5bb85a468d3c6bba92cf98
Britt Flowers <brittflowersmusic@gmail.com>
2017-11-14 10:17:22 -0800

    Add search form to header in layout and update search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @categories = Category.where(['name LIKE ?', "%#{params[:query].downcase}%"])
-    render json: @categories
+    @posts = Post.where(['lower(title) LIKE ?', "%#{params[:query].downcase}%"])
+    render json: @posts
   end
 end
