ae0b415a22b3186c84f588c000af99bf7127373d
Caroline & Shannon Nolan <pair+carolyoon.shannonnolan17@devbootcamp.com>
2017-11-15 17:42:37 -0800

    Test functionality

--- app/models/zip_code_adapter.rb
+++ app/models/zip_code_adapter.rb
@@ -1,8 +1,22 @@
 module ZipCodeAdapter
+  include HTTParty
 
-include HTTParty
+  base_uri "zipcodeapi.com/rest"
 
-base_uri "zipcodeapi.com/rest"
+  # def initialize(zip_code, distance)
+  #   res = self.get("/#{ENV['ZIP_API_TOKEN']}/radius.json/#{zip_code}/#{distance}/mile")
+
+  #   @data = res.parsed_response
+  # end
+
+  # def zip_codes
+  #   @data.map { |zip_code| zip_code['zip_code'] }
+  # end
+
+  # def cities_for(zip_codes)
+
+  # end
+
 
   def self.zip_search(zip_code, distance)
     res = self.get("/#{ENV['ZIP_API_TOKEN']}/radius.json/#{zip_code}/#{distance}/mile")
