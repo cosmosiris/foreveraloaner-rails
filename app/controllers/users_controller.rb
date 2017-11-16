6fb7598bc549080ec525251f24b6d03033d491ce
ikn02010 <ikn02010@gmail.com>
2017-11-15 20:09:52 -0800

    Add link to edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,14 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+	end
+
 
 	private
 
