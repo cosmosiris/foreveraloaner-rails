7c4db62fec7775f73580c722988d64b70961b648
ikn02010 <ikn02010@gmail.com>
2017-11-15 22:04:46 -0800

    Complete basic css for user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,6 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :email, :password, :user_name)
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :user_name, :bio)
 	end
 end
