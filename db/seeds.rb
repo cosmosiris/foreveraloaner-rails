853d22a21ac61a4130ecc32ff02c61b3526a7b3c
Caroline & Shannon Nolan <pair+carolyoon.shannonnolan17@devbootcamp.com>
2017-11-16 17:42:28 -0800

    Add data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,18 +19,73 @@ categories = ["Accessories", "Baby & Kid Products", "Beauty Products", "Books", 
 
 categories.map! { |category| Category.create(name: category) }
 
+garden = Tag.create!(name: "garden")
+tools = Tag.create!(name: "tools")
 #Farm and Garden
-Post.create!(title: "100 Series John Deere Lawn Mower", description: "You can borrow my 2016 100 Series John Deere riding mower and it is in great condition and can mow 1/3 of an acre up to 2 acres.", price: "$100/day", status: "open", zip_code: "98116", city: "Seattle", negotiable: true, loaner_id: 1, category: farm_and_garden)
-Post.create!(title: "Just Your Average Garden Hoe", description: "Thick blade with sharp edge for strength and clean cuts, fiberglass handle", price: "$5/day", status: "open", zip_code: "18929", city: "Jamison", negotiable: false, loaner_id: 2, category: farm_and_garden)
-Post.create!(title: "18 inch 40.2cc Gas Chainsaw", description: "Professional-grade 18 in. bar cuts up to 36 in. diameter logs.", price: "$70/day", status: "open", zip_code: "98116", city: "Seattle", negotiable: false, loaner_id: 3, category: farm_and_garden)
-Post.create!(title: "Leaf Blower", description: "Cordless jet fan leaf blower", price: "$50/day", status: "open", zip_code: "18929", city: "Jamison", negotiable: true, loaner_id: 4, category: farm_and_garden)
-Post.create!(title: "Pressure Washer", description: "Electric pressure washer, lightweight and compact. Lightly used.", price: "$40/day", status: "open", zip_code: "98116", city: "Seattle", negotiable: false, loaner_id: 3, category: farm_and_garden)
+lawn_mower = Post.create!(title: "100 Series John Deere Lawn Mower", description: "You can borrow my 2016 100 Series John Deere riding mower and it is in great condition and can mow 1/3 of an acre up to 2 acres.", price: "$100/day", status: "open", zip_code: "98116", city: "Seattle", negotiable: true, loaner_id: 1, category: farm_and_garden)
+lawn_mower.tags.create!(name: "lawn mower")
+lawn_mower.tags << garden
+lawn_mower.tags.create!(name: "riding mower")
+lawn_mower.tags.create!(name: "mow")
+
+
+hoe = Post.create!(title: "Just Your Average Garden Hoe", description: "Thick blade with sharp edge for strength and clean cuts, fiberglass handle", price: "$5/day", status: "open", zip_code: "18929", city: "Jamison", negotiable: false, loaner_id: 2, category: farm_and_garden)
+hoe.tags.create!(name: "gardening")
+hoe.tags << garden
+hoe.tags.create!(name: "hoe")
+hoe.tags.create!(name: "sharp")
+
+
+chainsaw = Post.create!(title: "18 inch 40.2cc Gas Chainsaw", description: "Professional-grade 18 in. bar cuts up to 36 in. diameter logs.", price: "$70/day", status: "open", zip_code: "98116", city: "Seattle", negotiable: false, loaner_id: 3, category: farm_and_garden)
+chainsaw.tags.create!(name: "chainsaw")
+chainsaw.tags.create!(name: "chopping")
+chainsaw.tags << tools
+
+
+leaf_blower = Post.create!(title: "Leaf Blower", description: "Cordless jet fan leaf blower", price: "$50/day", status: "open", zip_code: "18929", city: "Jamison", negotiable: true, loaner_id: 4, category: farm_and_garden)
+leaf_blower.tags.create(name: "leaf blower")
+leaf_blower.tags.create(name: "fall tools")
+leaf_blower.tags << tools
+leaf_blower.tags.create(name: "cordless")
+
+
+pressure_washer = Post.create!(title: "Pressure Washer", description: "Electric pressure washer, lightweight and compact. Lightly used.", price: "$40/day", status: "open", zip_code: "98116", city: "Seattle", negotiable: false, loaner_id: 3, category: farm_and_garden)
+pressure_washer.tags << tools
+pressure_washer.tags.create!(name: "lightweight")
+pressure_washer.tags.create!(name: "pressure washer")
+pressure_washer.tags.create!(name: "compact")
 
 #Party Supplies
-Post.create!(title: "Refillable Helium Tank", description: "Great for filling up balloons for your kid's birthday party. Comes empty.", price: "$5/day", status: "open", zip_code: "98116", city: "Seattle", negotiable: true, loaner_id: 5, category: party_supplies)
-Post.create!(title: "Tiki Torches (6)", description: "Crowd pleaser for your next tropically themed party.  Great condition!", price: "$5/day", status: "open", zip_code: "18929", city: "Jamison", negotiable: false, loaner_id: 2, category: farm_and_garden)
-Post.create!(title: "18 inch 40.2cc Gas Chainsaw", description: "Professional-grade 18 in. bar cuts up to 36 in. diameter logs.", price: "$70/day", status: "open", zip_code: "98116", city: "Seattle", negotiable: false, loaner_id: 3, category: farm_and_garden)
-Post.create!(title: "Leaf Blower", description: "Cordless jet fan leaf blower", price: "$50/day", status: "open", zip_code: "18929", city: "Jamison", negotiable: true, loaner_id: 4, category: farm_and_garden)
-Post.create!(title: "Pressure Washer", description: "Electric pressure washer, lightweight and compact. Lightly used.", price: "$40/day", status: "open", zip_code: "98116", city: "Seattle", negotiable: false, loaner_id: 3, category: farm_and_garden)
+party = Tag.create!(name: "party")
+
+tank = Post.create!(title: "Refillable Helium Tank", description: "Great for filling up balloons for your kid's birthday party. Comes empty.", price: "$5/day", status: "open", zip_code: "98116", city: "Seattle", negotiable: true, loaner_id: 5, category: party_supplies)
+tank.tags << party
+tank.tags.create!(name: "helium tank")
+tank.tags.create!(name: "balloons")
+
+tiki = Post.create!(title: "Tiki Torches (6)", description: "Crowd pleaser for your next tropically themed party.  Great condition!", price: "$5/day", status: "open", zip_code: "18929", city: "Jamison", negotiable: false, loaner_id: 2, category: party_supplies)
+tiki.tags << party
+tiki.tags.create!(name: "tiki torch")
+tiki.tags.create!(name: "tropical")
+tiki.tags.create!(name: "hawaiian")
+
+tent = Post.create!(title: "Party Tent for 20 People", description: "Plain white party tent for your next event, clean and easy to setup.", price: "$100/day", status: "open", zip_code: "98116", city: "Seattle", negotiable: true, loaner_id: 3, category: party_supplies)
+tent.tags << party
+tent.tags.create!(name: "tent")
+tent.tags.create!(name: "large tent")
+tent.tags.create!(name: "party tent")
+
+keg = Post.create!(title: "Empty Beer Keg", description: "1/2 barrel empty beer keg ready to be filled with your favorite beer.", price: "$80/day", status: "open", zip_code: "18929", city: "Jamison", negotiable: true, loaner_id: 4, category: party_supplies)
+keg.tags << party
+keg.tags.create!(name: "beer")
+keg.tags.create!(name: "keg")
+keg.tags.create!(name: "barrel")
+
+disco = Post.create!(title: "Disco Ball", description: "Large disco ball for your 70's themed party.", price: "$10/day", status: "open", zip_code: "18929", city: "Jamison", negotiable: false, loaner_id: 1, category: party_supplies)
+disco.tags << party
+disco.tags.create!(name: "dance")
+disco.tags.create!(name: "70s")
+disco.tags.create!(name: "lights")
+
 
 
